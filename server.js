2265f12093661d0e0d6e594753830f6262fb9d2c
Bindumuniyappa <muniyappabindu7@gmail.com>
2023-08-28 16:40:20 +0530

    changes to server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ let port = process.env.PORT;
 const app = express();
 
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors({ origin: "*" }));
 
 app.use("/", AuthRoute);
 app.use("/", ProjectRoute);
